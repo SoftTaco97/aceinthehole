f39ab503d0044a0a8149271e279911f33a6e273f
Jared <jared97martinez@gmail.com>
2019-02-11 16:55:35 -0800

    Switching template files

---
+++ includes/footer.php
@@ -0,0 +1,65 @@
+    
+    
+    <!-- FOOTER STARTS HERE -->
+    <footer>
+        <!-- .container-fluid -->
+        <section class="container-fluid">
+            <!-- .row -->
+            <div class="row">
+                <!-- .col -->
+                <div class="col-sm-3 offset-md-1 animateBlock left notAnimated">
+                    <h2 class="text-center">H2 Heading</h2>
+                    <p class="text-center">
+                        paragraph text paragraph text paragraph text paragraph text paragraph text paragraph text 
+                    </p>
+                </div>
+                <!-- /.col -->
+                <!-- .col -->
+                <div class="col-sm-4 text-center animateBlock left notAnimated">
+                    <i class="fab fa-facebook-square"></i>
+                    <i class="fab fa-twitter-square"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+                <!-- /.col -->
+                <!-- .col -->
+                <div class="col-sm-3 text-center animateBlock left notAnimated">
+                    <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Events</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">FAQs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Course</a>
+                            </li>
+                    </ul>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+            <!-- .row -->
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2 class="text-center">H2 Heading <span class="year"></span></h2>
+                </div>
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.container-fluid -->
+    </footer>
+    <!-- FOOTER ENDS HERE -->
+
+    <!-- JavaScript Links -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
+    <script src="js/animate.js"></script>
+    <script src="js/weather.js"></script>
+    <script src="js/script.js"></script>
+    
