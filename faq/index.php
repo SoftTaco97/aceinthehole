4b8cb6f4ee69f23bb14ab6a24e22e0de83139c83
Jared <jared97martinez@gmail.com>
2019-02-26 21:12:53 -0800

    FAQ page

--- faq/index.php
+++ faq/index.php
@@ -8,32 +8,61 @@
     <!-- MAIN STARTS HERE -->
     <main>
         <!-- .container-fluid -->
-        <section class="container-fluid">
-            <!-- .row -->
-            <div class="row">
-                <!-- .col-xs-10 -->
-                <div class="col-xs-10">
-                    <h2 class="text-center animateBlock notAnimated left">H2 Heading</h2>
-                    <p class="animateBlock notAnimated left">
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                    </p>
-                </div>
-                <!-- .col-xs-10 -->
+        <section class="container-fluid" id="faq">
+            <h3 class="text-center">What are the Rules?</h3>
+            <div>
+                <p class="text-center">
+                    We currently adhere to the USAT Rules for Triathlon & Duathlon.  
+                    Important rules include no drafting, you <strong>must</strong> wear a helmet and music is <strong>ABSOLUTELY NOT</strong> allowed during the bike for obvious safety reasons.
+                </p>
             </div>
-            <!-- /.row -->
+            <h3 class="text-center ">Can I use a personal music device while cycling?</h3>
+            <div>
+                <p class="text-center">
+                    Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.
+                </p>
+            </div>
+            <h3 class="text-center">Can I use a personal music device while running?</h3>
+            <div>
+                <p class="text-center">
+                    During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and<br> assure athletes can hear all course marshal instructions. 
+                    <br><br>
+                    With that said, we also understand that many athletes rely on music to help endure the challenge of running. 
+                    To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate:
+
+                    If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. 
+                    In addition, if you opt to use music, you will not be eligible for awards, points and rankings.<br>
+                    So as an athlete, you have to make a choice: music or awards/points/rankings.
+                    <br><br>
+                    <strong>If you are using a personal music devise with two earbuds, you will be immediately disqualified.</strong><br>
+                    Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.
+                    <br><br>
+                    We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.
+                </p>
+            </div>
+            <h3 class="text-center">Do I need to wear a wetsuit?</h3>
+            <div>
+                <p class="text-center">
+                No, you do not need to wear a wetsuit. <br><br>
+                Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor. 
+                </p>
+            </div>
+            <h3 class="text-center">Do I have to use a road or racing bike?</h3>
+            <div>
+                <p class="text-center">
+                    No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars. 
+                </p>
+            </div>
         </section>
         <!-- /.container-fluid -->
     </main>
     <!-- MAIN ENDS HERE -->
     <?php include_once('../includes/footer.php'); ?>
+    <script src="<?php echo FULL_PATH . '/js/jquery-ui.min.js'; ?>"></script>
+    <script>
+        $(function(){
+            $('#faq').accordion({ heightStyle: "content" });
+        });
+    </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -13,18 +13,11 @@
             <div class="row">
                 <!-- .col-xs-10 -->
                 <div class="col-xs-10">
-                    <h2 class="text-center animateBlock notAnimated left">H2 Heading</h2>
+                    <h2 class="text-center animateBlock notAnimated left">About The Event</h2>
                     <p class="animateBlock notAnimated left">
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
-                        paragraph text paragraph text paragraph text paragraph text 
+                    The Annual Ace in the Hole MultiSport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation. 
+
+                    There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!
                     </p>
                 </div>
                 <!-- .col-xs-10 -->
