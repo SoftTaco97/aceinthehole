20c8520312aa6f1084c69c3539de7327f72c3be7
Jared <jared97martinez@gmail.com>
2019-02-27 18:12:42 -0800

    Contact Form

--- index.php
+++ index.php
@@ -23,10 +23,43 @@
                 <!-- .col-xs-10 -->
             </div>
             <!-- /.row -->
+
+            <!-- .row -->
+            <div class="row">
+                <!-- .col-sm-10 -->
+                <div class="col-sm-10 text-center card card-body">
+                    <h2 class="text-center animateBlock notAnimated left">Contact</h2>
+                    <!-- form -->
+                    <form class="animateBlock notAnimated left" id="contactForm">
+                        <div class="form-group">
+                            <label for="name" class="sr-only">First and Last Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="First and Last Name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="telephone" class="sr-only">Telephone Number</label>
+                            <input type="telephone" class="form-control" id="telephone" placeholder="Phone number">
+                        </div>
+                        <div class="form-group">
+                            <label for="message" class="sr-only">Message</label>
+                            <textarea class="form-control" id="message" rows="3" placeholder="Hi! We love your events..."></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <small class="form-text text-muted">We'll never share your information with anyone else.</small>
+                    </form>
+                    <!-- /form -->
+                </div>
+                <!-- /.col-sm-10 -->
+            </div>
+            <!-- /.row -->
         </section>
         <!-- /.container-fluid -->
     </main>
     <!-- MAIN ENDS HERE -->
     <?php include_once('includes/footer.php'); ?>
+    <script src="<?php echo FULL_PATH . 'js/form.js'; ?>"></script>
 </body>
 </html>
